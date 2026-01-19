8ff261aa06fc641557b0ef7126037cde0101972c
Jesse de Poot <jjar.depoot@student.avans.nl>
2026-01-19 20:13:32 +0100

    Plantselector Test

--- Front End Jesse/smartplanter-jesse/vue.config.js
+++ Front End Jesse/smartplanter-jesse/vue.config.js
@@ -1,15 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
-  transpileDependencies: true,
-  devServer: {
-    proxy: {
-      '/api': {
-        target: 'https://smartplanters.dedyn.io:1880',
-        changeOrigin: true,
-        secure: false, // belangrijk bij HTTPS backend zonder volledig certificaat
-        pathRewrite: path => path.replace(/^\/api/, '/smartplantdata'),
-      },
-    },
-  },
+  transpileDependencies: true
 })
