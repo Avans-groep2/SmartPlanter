a8889eae3967fba1b5785284f3b0afc552bba9ec
Immepiels07 <ima.piels@student.avans.nl>
2026-01-21 13:52:24 +0100

    meldingen test met API

--- Front End Imme/smartplanter-imme/src/pages/Notificaties.vue
+++ Front End Imme/smartplanter-imme/src/pages/Notificaties.vue
@@ -2,10 +2,54 @@
 
   <div class="belangrijke_meldingen">
     <h1 style="color:red;" class="notificatiesH1">Belangrijke meldingen!</h1>
-    <p class="meldingenLaden">Hier worden meldingen geladen...</p></div>
-  <div class="overige_meldingen">
-    <h1 class="notificatiesH1">Overige meldingen</h1>
-    <p class="meldingenLaden">Hier worden meldingen geladen...</p></div>
+    <p v-if="loading" class="meldingenLaden">Meldingen wordeen geladen...</p>
+  
+    <table v-else-if="belangrijkeMeldingen.length" class="meldingen-tabel">
+      <thead>>
+        <tr>
+          <th>Moestuin</th>
+          <th>Melding</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="melding in belangrijkeMeldingen" 
+        :key="melding.MeldingID"
+        class="prioriteit-hoog">
+          <td>{{ melding.DeviceID }}</td>
+          <td>{{ melding.Bericht }}</td>
+          <td class="verwijder-cel">
+            <button class="verwijderKnop">Verwijderen</button></td>
+        </tr>
+      </tbody>
+    </table>
+      <p v-else class="geen-meldingen">Geen belangrijke meldingen.</p>    
+    </div>
+
+    <div class="overige_meldingen">
+      <h1 class="notificatiesH1">Overige meldingen</h1>
+      <p v-if="loading" class="meldingenLaden">Meldingen worden geladen...</p>
+    
+    <table v-else-if="overigeMeldingen.length" class="meldingen-tabel">
+      <thead>
+        <tr>
+          <th>Moestuin</th>
+          <th>Melding</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="melding in overigeMeldingen"
+        :key="melding.MeldingID">
+          <td>{{ melding.DeviceID }}</td>
+          <td>{{ melding.Bericht }}</td>
+          <td class="verwijder-cel">
+            <button class="verwijderKnop">Verwijderen</button></td>
+        </tr>
+      </tbody>
+    </table>
+      <p v-else class="geen-meldingen">Geen overige meldingen.</p>
+    </div>
     
     <div v-if="isBeheerder" class="meldingenDropdownAdmin">
         <div class="moestuinKeuzeDropDown" ref="dropdown">
@@ -25,15 +69,14 @@
     </div>
 
     <div class="inspiratieKnop"> 
-    <a href="https://www.keukenliefde.nl/kook-koelkast-leeg/" class="inspiraiteWebsite" style="color:white";>? </a>
-  </div>
-
+      <a href="https://www.keukenliefde.nl/kook-koelkast-leeg/" class="inspiraiteWebsite" style="color:white";>? </a>
+    </div>
 </template>
 
 
 
 <script>
-import { computed } from 'vue';
+import { ref, onMounted, computed } from 'vue';
 import { useFooterSpan } from '../stores/footerSpan';
 import { useMoestuinStore } from '../stores/moestuinScherm';
 
@@ -44,23 +87,51 @@ export default {
     const footerStore = useFooterSpan();
     const moestuinStore = useMoestuinStore();
 
+    const meldingen = ref([]);
+    const loading = ref(true);
+
     const isBeheerder = computed(() => {
       if (!footerStore.keycloak) return false;
-
       return footerStore.keycloak.hasRealmRole('beheerder') ||
-        footerStore.keycloak.hasResourceRole('beheerder', 'frontend-imme');
+        footerStore.keycloak.hasResourceRole('beheerder', 'frontend-imme'); 
     });
 
-    return { moestuinStore, isBeheerder, footerStore };
+    const fetchMeldingen = async () => {
+      try{
+        loading.value = true;
+        const response = await fetch('https://smartplanters.dedyn.io:1880/smartplantdata');
+        const data = await response.json();
+        meldingen.value = data;
+      } catch (error) {
+        console.error("Er konden geen meldingen opgehaald worden:", error);
+      } finally {
+        loading.value = false;
+      }
+    };
+
+    const belangrijkeMeldingen = computed(() => {
+      return meldingen.value.filter(m => m.Prioriteit === 'hoog');
+    });
+
+    const overigeMeldingen = computed(() => {
+      return meldingen.value.filter(m => m.Prioriteit !== 'hoog');
+    });
+
+    onMounted(() => {
+      fetchMeldingen();
+      setInterval(fetchMeldingen, 60000);
+    })
+
+
+    return { moestuinStore, isBeheerder, footerStore, meldingen, 
+      loading, belangrijkeMeldingen, overigeMeldingen};
   },
 
-  data() {
+  data(){
     return {
-      meldingen: [],
-      loading: true,
       open: false,
       moestuinen: ['Moestuin 1', 'Moestuin 2', 'Moestuin 3']
-    };
+    }
   },
 
   computed: {
@@ -76,45 +147,82 @@ export default {
 
     selecteerMoestuin(moestuin) {
       this.moestuinStore.setMoestuin(moestuin);
-      this.open = false;
+      this.open = false; 
     },
 
-    handleClickOutside(event) {
+     handleClickOutside(event) {
       if (this.$refs.dropdown && !this.$refs.dropdown.contains(event.target)) {
         this.open = false;
       }
     },
-
-    async haalMeldingenOp() {
-      try {
-        const response = await fetch('https://smartplanters.dedyn.io:1880/smartplantdata');
-        const data = await response.json();
-        console.log('API response:', data);
-        this.meldingen = data;
-      } catch (err) {
-        console.error('Fout bij ophalen meldingen', err);
-      } finally {
-        this.loading = false;
-      }
-    }
   },
 
   mounted() {
     document.addEventListener("click", this.handleClickOutside);
-    this.haalMeldingenOp();
   },
 
   beforeUnmount() {
     document.removeEventListener("click", this.handleClickOutside);
   }
-};
+}
 </script>
 
 
 
 
-
 <style>
+/* Tabel styling om te lijken op de foto */
+.meldingen-tabel {
+  width: 100%;
+  border-collapse: collapse;
+  margin-top: 10px;
+}
+
+.meldingen-tabel th {
+  text-align: left;
+  padding: 8px;
+  border-bottom: 2px solid #ddd;
+  font-weight: 600;
+}
+
+.meldingen-tabel td {
+  padding: 10px 8px;
+  border-bottom: 1px solid #eee;
+}
+
+/* Groene rij voor belangrijke meldingen zoals op foto */
+.prioriteit-hoog {
+  background-color: #2d6a4f;
+  color: white;
+}
+
+.verwijder-cel {
+  text-align: right;
+  width: 40px;
+}
+
+.verwijder-btn {
+  background: none;
+  border: none;
+  cursor: pointer;
+  font-size: 1.2rem;
+}
+
+.geen-meldingen {
+  margin-top: 10px;
+  color: #666;
+  font-style: italic;
+}
+
+/* Behoud je bestaande styles voor de containers... */
+.belangrijke_meldingen, .overige_meldingen {
+  overflow-y: auto; /* Zorg dat je kunt scrollen als er veel meldingen zijn */
+}
+
+
+
+
+
 
 .meldingenDropdownAdmin {
   margin-bottom: 5px;
