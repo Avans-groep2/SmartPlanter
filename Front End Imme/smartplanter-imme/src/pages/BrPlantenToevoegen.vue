7891ac8b8e01e84c406fe6f77b7283caab516c1d
Immepiels07 <ima.piels@student.avans.nl>
2026-01-27 15:36:21 +0100

    test read planten info

--- Front End Imme/smartplanter-imme/src/pages/BrPlantenToevoegen.vue
+++ Front End Imme/smartplanter-imme/src/pages/BrPlantenToevoegen.vue
@@ -5,24 +5,67 @@
         <table class="plantToevoegTabel">
             <thead>
                 <tr>
-                    <th>test</th>
-                    <th>Test2</th>
+                    <th>PlantID</th>
+                    <th>PlantSoort</th>
+                    <th>PhMin</th>
+                    <th>PhMax</th>
+                    <th>Groeitijd</th>
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td>Invul</td>
-                    <td>Invul2</td>
+                <tr v-for="plant in plantInfo"
+                    :key="plant.PlantID">
+                    <td>{{ plant.PlantID }}</td>
+                    <td>{{ plant.PlantSoort }}</td>
+                    <td>{{ plant.PhMin }}</td>
+                    <td>{{ plant.PhMax }}</td>
+                    <td>{{ plant.Groeitijd }}</td>
                 </tr>
-            </tbody>
+                <tr v-if="plantInfo.length === 0">
+                    <td colspan="5">Laden...</td>
+                </tr>
+            </tbody> 
         </table>
     </div>
 
 </template>
 
 <script>
+import { useFooterSpan } from '@/stores/footerSpan';
+import { useMoestuinStore } from '@/stores/moestuinScherm';
+import { computed, ref, onMounted } from 'vue';
+
 export default {
   name: 'PlantToevoegenAdminPagina',
+
+  setup() {
+    const footerStore = useFooterSpan();
+    const moestuinStore = useMoestuinStore();
+    const loading = ref(false);
+
+    const plantInfo = computed (() => moestuinStore.plantInfo);
+
+    const fetchPlantInfo = async () => {
+      try{
+        loading.value = true;
+        const response = await fetch('https://smartplanters.dedyn.io:1880/smartplantdata?table=Planten');
+        const data = await response.json();
+        
+        moestuinStore.plantInfo = Array.isArray(data) ? data : [];
+      } catch (error) {
+        console.error(error);
+        plantInfo.value = [];
+      } finally {
+        loading.value = false;
+      }
+    };
+
+    onMounted(() => {
+        fetchPlantInfo();
+    });
+
+    return {moestuinStore, footerStore, plantInfo, loading};
+}
 }
 
 
@@ -54,6 +97,7 @@ export default {
     background: white;
     padding: 20px;
     width: 90%;
+    margin-top: 2rem;
     margin-left: 5%;
     border-radius: 10px;
     margin-bottom: 20px;
